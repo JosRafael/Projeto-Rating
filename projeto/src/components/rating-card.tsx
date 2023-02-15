0437fa5b50b359610171fff53c63536f741ede6d
JRafael <rafaelmatiashue@gmail.com>
2023-02-15 10:36:48 -0300

    config rating-card

---
+++ projeto/src/components/rating-card.tsx
@@ -0,0 +1,11 @@
+import styles from './rating-card.module.css';
+export default function RatingCard(){
+    return <div className={styles.card}>
+        
+        <h1>How did we do?</h1>
+
+        <p>
+            Please let us know how we did with your support request. Aprecciated help us improve our offering!
+        </p>
+    </div>
+}
