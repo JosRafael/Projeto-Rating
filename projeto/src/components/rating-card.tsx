3858a9d2ab34cbccaab9f8c39b1b557b6b3d3299
JRafael <rafaelmatiashue@gmail.com>
2023-02-15 11:21:43 -0300

    andamento button

--- projeto/src/components/rating-card.tsx
+++ projeto/src/components/rating-card.tsx
@@ -4,18 +4,22 @@ export default function RatingCard(){
        <div>
         <img className={styles.star} src='/icon-star.svg' alt='star'/>
        </div>
-        
+
+       <div className={styles.text}>
         <h1 className={styles.title}>How did we do?</h1>
-
         <p className={styles.description}>
-            Please let us know how we did with your support request. Aprecciated help us improve our offering!
+            Please let us know how we did with your support request. all feedbacks is 
+            aprecciated help us improve our offering!
         </p>
-
+        </div> 
+       
+        <div>
         <button className={styles.button}>1</button>
         <button className={styles.button}> 2</button>
         <button className={styles.button}>3</button>
         <button className={styles.button}>4</button>
         <button className={styles.button}>5</button>
+        </div>
         <button className={styles.submit}>submit</button>
     </div>
 }
